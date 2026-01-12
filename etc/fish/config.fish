a3c3a85d9a03685f70f9864b22fdf46c0e63e2ee
Flareon <flareon1006@proton.me>
2026-01-12 17:22:37 +0100

    Update config.fish

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
-alias maj='paru && flatpak update'
+alias maj='paru && flatpak upgrade'
 alias nvgpu='env __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only'
 end
 
